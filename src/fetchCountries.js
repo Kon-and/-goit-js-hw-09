a9af3c0b00b7d0f5cba138cfb9491a94b8f6131a
Kon-and <kosten.andr@gmail.com>
2022-09-22 00:46:43 +0300

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -3,7 +3,7 @@ const BASE_URL = 'https://restcountries.com/v3.1';
 function fetchCountries(name, error) {
   return fetch(`${BASE_URL}/name/${name}`).then(res => {
     if (!res.ok) {
-      // throw new Error(res.status);
+      throw new Error(res.status);
       return error();
     }
     return res.json();
